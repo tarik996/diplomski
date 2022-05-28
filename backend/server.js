4ba2ea9219a835564547cbbec3d605dea60bdcb6
tarik996 <tkulosmano1@etf.unsa.ba>
2022-05-28 14:55:34 +0200

    Izmjena cors

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://tarik-diplomski.herokuapp.com"],
     credentials: true
 }));
 
