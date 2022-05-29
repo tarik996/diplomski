2c7588bea93ec269ef6968d2b51af920dcd7e794
tarik996 <tkulosmano1@etf.unsa.ba>
2022-05-29 17:04:52 +0200

    Ispravka puta na server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cors({
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
-    app.get("*", (req, res) => { res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html")); }); 
+    app.get("*", (req, res) => { res.sendFile(path.resolve(__dirname, "frontend/build", "index.html")); }); 
 }
 
 //Route middlewares
